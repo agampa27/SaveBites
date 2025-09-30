4ff7f9b1db7901af8c104086de10f1567a473cca
ddale314 <dylandale999@gmail.com>
2025-09-29 22:02:58 -0700

    notes, userprofile class

---
+++ Main.py
@@ -0,0 +1,8 @@
+# test
+from UserProfile import UserProfile
+
+user_id = -1
+past_purchases = {"tomato": 1}
+dietary_restrictions = {"gluten"}
+
+test_user = UserProfile()
---
+++ UserProfile.py
@@ -0,0 +1,29 @@
+class UserProfile():
+    
+    # similar to static class variables in Java, shared by all instances of this class
+    class_user_id = 1
+
+    def __init__(self, dietary_restrictions, cuisine_preferences, budget, time_available, appliances):
+
+        # auto generate unique user id for all users
+        self.user_id = f"{UserProfile.class_user_id:05d}"
+        UserProfile.class_user_id += 1
+
+        # set ()
+        self.dietary_restrictions = dietary_restrictions
+        # 
+        self.cusine_preferences = cuisine_preferences
+        # int 
+        self.budget = budget
+        # int
+        self.time_available = time_available
+        # set ()
+        self.appliances = appliances
+        
+        # { ingredient : { "quantity" : count, "expiration date" : datetime object }} 
+        self.pantry = {}
+        # [ ingredient ]
+        self.purchase_history = []
+        # [ { recipe : rating} ]
+        self.recipe_ratings = []
+
