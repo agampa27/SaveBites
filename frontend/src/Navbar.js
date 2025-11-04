367cc5d40fb66351698fa558eee6a116fbf1471b
ddale314 <dylandale999@gmail.com>
2025-11-04 11:04:08 -0800

    fixed packages, navbar, links

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,11 +1,20 @@
+import { Link } from "react-router-dom"
+
 export default function NavBar() {
 	return (
 		<>
-			<nav className ="navbar" 
-                style={{position : 'sticky', top : 0}}>
-				Test
-				<a href="/Login"></a>
-                <button> Sign Up </button>
+			<nav className ="navbar" style={{position : 'sticky', top : 0, justifyContent: "space-between"}}>
+				<div style={{backgroundColor: "rgba(50, 200, 50, 0.5)"}}>
+					<div style={{textAlign: "left"}}>
+						<Link to="/">SaveBites</Link>
+					</div>
+					<div style={{textAlign: "right"}}>
+						<Link to="/Login">Log In</Link>
+					</div>
+					<div style={{textAlign: "right"}}>
+						<Link to="/Signup">Sign Up</Link>
+					</div>
+				</div>
 			</nav>
 		</>
 	)
