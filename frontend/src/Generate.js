e5154654ede792ea638c2c1fb39e438de97a0a4b
ddale314 <dylandale999@gmail.com>
2025-11-14 12:59:18 -0800

    recipe generation test

--- frontend/src/Generate.js
+++ frontend/src/Generate.js
@@ -1,6 +1,33 @@
+import { useState } from 'react';
+
 export default function Generate() {
-	const testIngredients = ["Tomato", "Ground Beef"]
+	const [recipes, setRecipes] = useState([]);
+
+	const testIngredients = ["Tomato", "Ground Beef", "Cheese", "Flour", "Herbs", "Egg", "Bell Peppers"]
 	const testCuisines = ["Italian", "American"]
+
+	function generateRecipe() {
+		let testRecipe = {
+			ingredients: "Tomato, Cheese, Flour",
+			instructions: "Make pizza, put in oven",
+			appliances: "Oven",
+			budget: "10"
+		};
+
+		setRecipes(recipes.concat(testRecipe));
+	}
+
+	function recipeDisplay(recipe) {
+		return (
+			<p>
+				Ingredients: {recipe.ingredients}  <br />
+				Instructions: {recipe.instructions} <br />
+				Appliances: {recipe.appliances} <br />
+				Budget: ${recipe.budget}
+			</p>
+		);
+	}
+
 	return (
 		<p style={{position: "relative", left: "50px"}}>
 			<h1>Generate Recipes!</h1>
@@ -31,7 +58,15 @@ export default function Generate() {
 				</ul>
 			</p>
 
-			<button>Generate</button>
+			<button onClick={generateRecipe}>Generate</button>
+
+			<p style={{position: "relative", left: "50px"}}>
+				{recipes.map( (item) => 
+					{
+						return recipeDisplay(item);
+					}
+				)}
+			</p>
 		</p>
 	);
 }
