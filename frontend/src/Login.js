212d1c217356e888f513180f96a841f04607a7e8
ddale314 <dylandale999@gmail.com>
2025-11-21 18:20:14 -0800

    login/signup styling

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,31 +1,36 @@
 import { Link } from 'react-router-dom';
+import "./Login.css";
 
 export default function Login() {
 	return (
-		<>
-			<h1>Welcome Back!</h1>
-			<p>Enter your credentials:</p>
+		<div className="login-page-container">
+			<div className="login-form-box">
+			<h1 className="login-title">Welcome Back!</h1>
+			<p className="login-subtitle">Enter your credentials:</p>
 
-			<div>
-				<label>
-					Email address <br />
-					<input type="email" placeholder="Enter your email"></input>
-				</label>
-			</div>
-			
-			<div>
-				<label>
-				Password <br />
-				<input type="password" placeholder="Enter your password"></input>
-				</label>
-			</div>
+			<form className="login-form">
+				<div className="form-group">
+					<label>
+						Email address <br />
+						<input className="form-input" type="email" placeholder="Enter your email"></input>
+					</label>
+				</div>
+				
+				<div className="form-group">
+					<label>
+					Password <br />
+					<input className="form-input" type="password" placeholder="Enter your password"></input>
+					</label>
+				</div>
 
-			<button>Login</button>
+				<button type="submit" className="btn-login">Login</button>
+			</form>
 
-			<p>
+			<p className="signup-prompt">
 				Don't have an account?&nbsp;
 				<Link to="/signup">Sign Up</Link>
 			</p>
-		</>
+			</div>
+		</div>
 	)
 }
--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,37 +1,48 @@
+import "./Signup.css";
+import { Link } from "react-router-dom";
+
 export default function Signup() {
 	return (
-		<>
-			<h1>Get Started Now</h1>
+		<div className="signup-page-container">
+			<div className="signup-form-box">
+				<h1 className="signup-title">Get Started Now</h1>
+				<form className="signup-form">
+					<div className="form-group">
+						<label>
+							Name <br />
+							<input className="form-input" placeholder="Name"></input>
+						</label>
+					</div>
 
-			<div>
-				<label>
-					Name <br />
-					<input placeholder="Name"></input>
-				</label>
+					<div className="form-group">
+						<label>
+							Email address <br />
+							<input className="form-input" type="email" placeholder="Enter your email"></input>
+						</label>
+					</div>
+
+					<div className="form-group">
+						<label>
+							Password <br />
+							<input className="form-input" type="password" placeholder="Enter a password"></input>
+						</label>
+					</div>
+
+					<div className="form-group">
+						<label>
+							Confirm Password <br />
+							<input className="form-input" type="password" placeholder="Confirm your password"></input>
+						</label>
+					</div>
+
+					<button type="submit" className="btn-signup">Sign Up</button>
+					
+				</form>
+				<p className="login-prompt">
+					Already have an account?&nbsp;
+					<Link to="/login">Log In</Link>
+				</p>
 			</div>
-
-			<div>
-				<label>
-					Email address <br />
-					<input type="email" placeholder="Enter your email"></input>
-				</label>
-			</div>
-
-			<div>
-				<label>
-					Password <br />
-					<input type="password" placeholder="Enter a password"></input>
-				</label>
-			</div>
-
-			<div>
-				<label>
-					Confirm Password <br />
-					<input type="password" placeholder="Confirm your password"></input>
-				</label>
-			</div>
-
-			<button>Sign Up</button>
-		</>
+		</div>
 	)
 }
